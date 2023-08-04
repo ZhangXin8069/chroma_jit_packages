0acf4d2202233f8f027310eba6d99842f9f19e6a
zhangxin <zhangxin8069@qq.com>
2023-08-04 18:46:43 +0800

    2023-08-04-18-46-43

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
+# source
+_PATH=$(
+    cd "$(dirname "$0")"
+    pwd
+)
+pushd ${_PATH}
 source ./env.sh
+popd
+
 
 pushd ${install_path}
-tar xzf ${file_path}/chroma_jit_packages && pushd ./chroma_jit_packages/install
+tar xzf ${file_path}/chroma_jit_packages.tar.gz && pushd ./chroma_jit_packages/install
 bash ./build_qmp.sh
 bash ./build_libxml2.sh
 bash ./build_llvm.sh
