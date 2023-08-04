8e0e59be020f76cfca8d8975f421eb4a914b6077
zhangxin <zhangxin8069@qq.com>
2023-08-04 19:46:41 +0800

    2023-08-04-19-46-41

--- env.sh
+++ env.sh
@@ -1,8 +1,3 @@
-file_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-# install_path=$(cd ${file_path}/../external-libraries && pwd) #absolute path
-install_path=${HOME}/external-libraries #absolute path
-mkdir ${install_path} -p
-
 # conda init && source ~/.bashrc
 # conda install git g++ gcc gfortran libopenblas-dev liblapack-dev libboost-dev \
 #     libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev \
@@ -26,5 +21,67 @@ CUDA_INSTALL_PATH=/public/soft/linux-centos7-x86_64/gcc-10.3.0/cuda-11.4.4-uizl3
 MPI_HOME=/public/soft/linux-centos7-x86_64/gcc-10.3.0/openmpi-4.1.5-5jp5lnxsmoilvbssatnylov53a7lcp2p/
 GCC_HOME=/public/soft/linux-centos7-x86_64/gcc-4.8.5/gcc-10.3.0-uoicdrf766usj4ma5wxqq4zaqgatfyy3/
 OMP="yes"
-SM=sm_86 # Kepler
+SM=sm_86          # Kepler
 MAKE="make -j 48" #rely on:lscpu,more is better
+
+#just change above 
+
+# The directory containing the build scripts, this script and the src/ tree
+TOPDIR=$(cd . && pwd) 
+#export CUDA_INSTALL_PATH=/usr/local/cuda-10.0
+export PATH=${GCC_HOME}/bin:${CUDA_INSTALL_PATH}/bin:${MPI_HOME}/bin:$PATH
+
+# Install directory
+SRCDIR=${TOPDIR}/../
+BUILDDIR=${TOPDIR}/build
+mkdir ${BUILDDIR}
+INSTALLDIR=${TOPDIR}/bin/${SM}
+if [ "x${OMP}x" == "xyesx" ]; then
+    INSTALLDIR=${INSTALLDIR}_omp
+fi
+LLVM_INSTALL_DIR=${INSTALLDIR}/llvm-nvptx
+export PATH=$PATH:${LLVM_INSTALL_DIR}/bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LLVM_INSTALL_DIR}/lib
+export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${LLVM_INSTALL_DIR}/include
+export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${LLVM_INSTALL_DIR}/include
+
+### ENV VARS for CUDA/MPI
+# These are used by the configure script to make make.inc
+PK_CUDA_HOME=${CUDA_INSTALL_PATH}
+PK_MPI_HOME=${MPI_HOME}
+PK_GPU_ARCH=${SM}
+
+### OpenMP
+# Open MP enabled
+if [ "x${OMP}x" == "xyesx" ]; then
+    OMPFLAGS="-fopenmp -D_REENTRANT"
+    #OMPFLAGS="-fopenmp"
+    OMPENABLE="--enable-openmp"
+else
+    OMPFLAGS=""
+    OMPENABLE=""
+fi
+if [ ! -d ${INSTALLDIR} ]; then
+    mkdir -p ${INSTALLDIR}
+fi
+
+### COMPILER FLAGS
+ARCHFLAGS="-march=native"
+DEBUGFLAGS="-g"
+#DEBUGFLAGS=""
+#PK_CXXFLAGS="${OMPFLAGS} ${ARCHFLAGS} ${DEBUGFLAGS} -O3 -std=gnu++11 -fexceptions -frtti -g"
+PK_CXXFLAGS="${OMPFLAGS} ${ARCHFLAGS} ${DEBUGFLAGS} -O3 -std=c++1y -fexceptions -frtti -fPIC"
+PK_CFLAGS="${OMPFLAGS} ${ARCHFLAGS} ${DEBUGFLAGS}  -O3 -std=gnu99 -fPIC"
+#PK_CFLAGS="${ARCHFLAGS} -O3 -std=gnu99 -g"
+# PK_LDFLAGS="-L${CUDA_INSTALL_PATH}/lib64 -L${CUDA_INSTALL_PATH}/lib64/stubs -L${CUDA_INSTALL_PATH}/nvvm/lib64"
+PK_LDFLAGS="-L${CUDA_INSTALL_PATH}/lib64 -L${CUDA_INSTALL_PATH}/lib64/stubs -L${CUDA_INSTALL_PATH}/nvvm/lib64"
+PK_LIBS="${PK_LDFLAGS}"
+
+### MPI and compiler choices
+PK_CC=mpicc
+PK_CXX=mpicxx
+PK_LLVM_CXX=g++
+PK_LLVM_CC=gcc
+# PK_LLVM_CFLAGS="-O3 -std=c99"
+PK_LLVM_CXXFLAGS=" -O3 -std=c++11"
+QDPJIT_HOST_ARCH="X86;NVPTX"
--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
-# source
-_PATH=$(
-    cd "$(dirname "$0")"
-    pwd
-)
-pushd ${_PATH}
-source ./env.sh
-popd
+file_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# install_path=$(cd ${file_path}/../external-libraries && pwd) #absolute path
+install_path=${HOME}/external-libraries #absolute path
+mkdir ${install_path} -p
 
 pushd ${install_path}
 tar xzf ${file_path}/chroma_jit_packages.tar.gz && pushd ./chroma_jit_packages/install
+cp ${file_path}/env.sh .
 bash ./build_qmp.sh
 bash ./build_libxml2.sh
 bash ./build_llvm.sh
