7ae5a4209b15b564ee3354318468cdadebcd0cec
zhangxin <zhangxin8069@qq.com>
2023-08-04 17:56:47 +0800

    2023-08-04-17-56-47

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-file_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+file_path=$(cd . && pwd)
 # install_path=$(cd ${file_path}/../external-libraries && pwd) #absolute path
 install_path=${HOME}/external-libraries #absolute path
 mkdir ${install_path} -p
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 source ./env.sh
 
 pushd ${install_path}
-tar xzf ${file_path}/chroma_jit_packages && pushd chroma_jit_packages/install
+tar xzf ${file_path}/chroma_jit_packages && pushd ./chroma_jit_packages/install
 bash ./build_qmp.sh
 bash ./build_libxml2.sh
 bash ./build_llvm.sh
